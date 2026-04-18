3d9f6b5c9aefc02cbbca8f47d5bbf45ac697d4bb
felat.aslan <felataslan58@gmail.com>
2026-04-18 19:31:47 +0300

    refactor: update view layouts to use _AdminLayout and adjust dashboard navigation link

--- obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996264a194faf915fc7ff6f8e50233c0a799ae5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
